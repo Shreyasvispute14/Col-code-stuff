372cf62bfcb086c5f7cf481cea92e5f4d4595e21
Shreyas vispute <shreyasvispute14@gmail.com>
2024-08-11 13:57:09 +0530

    day 5

---
+++ C-lang/temp.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float f,c,k;
+    printf("\n Enter desired value for Fahrenheit:");
+    scanf("%f",f);
+    c=(f-32)*5/9;
+    printf("\n Temperature in celsius is: %f",c);
+    k=c+273.5;
+    printf("\n Temperature in kelvin is: %f",k);
+     
+}
