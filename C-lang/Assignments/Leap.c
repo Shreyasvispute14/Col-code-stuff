540666c6f45c523d54522ae38b5805160d77953e
Shreyas vispute <shreyasvispute14@gmail.com>
2024-08-13 18:19:03 +0530

    day 8

--- C-lang/Assignments/Leap.c
+++ C-lang/Assignments/Leap.c
@@ -1,25 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int year;
-    printf("Enter year:");
-    scanf("%d", &year);
-    if (year % 400==0)
-    {
-        printf("%d is a leap year",year);
+    int y=2024;
+    if(y %4 == 0){
+        printf("The year %d is a leap year",y);
     }
-    else if (year % 100 == 0)
-    {
-        printf("%d is not a leap year",year);
+    else{
+        printf("The year %d is not a leap year",y);
     }
-    else if (year % 4 == 0)
-    {
-        printf("%d is not a leap year",year);
-    }
-    else
-    {
-        printf("%d is not a leap year",year);
-    }
+
     return 0;
 
 }
---
+++ C-lang/Assignments/Set_B/distance.c
@@ -0,0 +1,14 @@
+// Compute distance between X and Y coords.
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(){
+
+    float x,y,a,b,distance;
+    printf("\n Enter the X Coordinates value:");
+    scanf("%f",&x);
+    printf("\n Enter the Y Coordinates value:");
+    scanf("%f",&y);
+
+}
