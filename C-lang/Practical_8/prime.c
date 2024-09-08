fddd94148268958d0d68538c50f50bf3627a3b5e
Shreyas vispute <shreyasvispute14@gmail.com>
2024-09-08 23:04:39 +0530

    day16

---
+++ C-lang/Practical_8/prime.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int i,j,isprime;
+    for (i=2; i<=100; i++){
+        isprime = 1;
+        for (j=2; j<=i/2; j++){
+            if (i%j==0){
+                isprime = 0;
+                break;
+            }
+        }
+        if(isprime == 1){
+        printf("%d\t",i);
+        }
+    }
+    return 0;
+}
